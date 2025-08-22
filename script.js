0f81f5972bfed926cc6e23cd2fbac7dea4a93638
Krish Panchal <krishpanchal803@gmail.com>
2025-08-22 15:09:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("cuisinefile").onclick = function () {
+    window.open("cuisine.html", "_blank")
+}
+document.getElementById("artcraft").onclick = function () {
+    window.open("artcraft.html", "_blank")
+}
+
+document.getElementById("religioussite").onclick = function () {
+    window.open("religioussite.html", "_blank")
+}
+
+document.getElementById("heritagesite").onclick = function () {
+    window.open("heritagesite.html", "_blank")
+}
+
